b0d69f03a497f9f0b913d36521e2514958ee0d22
thomasorten <thomas.orten@gmail.com>
2014-10-02 16:27:38 +0200

    Day 4

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,7 +13,8 @@ angular.module('trafikklysApp')
 
         var marker = new google.maps.Marker({
             position: location, 
-            map: $scope.googleMap
+            map: $scope.googleMap,
+            icon: '/assets/images/bulb.png'
         });
 
         // add marker in markers array
@@ -53,7 +54,8 @@ angular.module('trafikklysApp')
             var marker = new google.maps.Marker({
               position: new google.maps.LatLng($scope.loadedLights[i].lat, $scope.loadedLights[i].lon),
               map: $scope.googleMap,
-              title: $scope.loadedLights[i].name
+              title: $scope.loadedLights[i].name,
+              icon: '/assets/images/bulb.png'
             }); 
 
             bindWindow(marker, i);  
@@ -86,6 +88,9 @@ angular.module('trafikklysApp')
                 loadMarkers();
               });
           }
+        },
+        options: {
+          styles: [{"featureType":"water","stylers":[{"color":"#021019"}]},{"featureType":"landscape","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"transit","stylers":[{"color":"#146474"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]}]
         }
     };
 
